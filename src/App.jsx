3d075738f460bbf30d2ed5a2bc83f96bd442d1cf
Necrosyth <acoustify69420@gmail.com>
2024-05-31 00:55:50 +0530

    added Functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,77 @@
 import React from 'react'
-import { useState,useCallback } from 'react';
+import { useEffect,useRef,useState, useCallback } from 'react';
 
 const App = () => {
 
-  const [length,setLength]=useState(8);
-  const [numberAllowed,setNumberAllowed]=useState(false)
-  const [charAllowed,setCharAllowed]=useState(false)  
-  const [password,setPassword]=useState("")
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
 
-  const passwordGenerator=useCallback(()=>{
-    let pass=""
-    let str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    if(numberAllowed) str+="0123456789"
-    if(charAllowed) str+="!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+  console.log(length)
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 
-    for (let i = 1; i <=length; i++){
-      let char=Math.floor(Math.random()*str.length+1)
-     pass+=str.charAt(str)  
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
 
     }
     setPassword(pass)
 
   }
-  ,[length,numberAllowed,charAllowed,setPassword])
+  
+    , [length, numberAllowed, charAllowed, setPassword])
+
+    useEffect(() => {
+      passwordGenerator()
+    }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
       <h1 className='text-white text-center my-3'>Password generator</h1>
-    <div className="flex shadow rounded-lg overflow-hidden mb-4">
-     </div>
-     </div>
-    
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input type="text" value={password} readOnly className='outline-none w-full px-5 py-3' placeholder='Password' />
+
+        <button className='bg-blue-800 px-3 shrink-0'>
+
+          Copy </button>
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div>
+          <input className='flex items-center gap-x-1' type="range" min={5} max={20} value={length} onChange={(e) => { setLength(e.target.value) }
+          }
+            id="" />
+          <label>Length: {length}</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev)
+            }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
