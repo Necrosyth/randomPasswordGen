24c0ce83f5bd0e579d409d1fcfe3e5f09a4c84b4
Necrosyth <acoustify69420@gmail.com>
2024-05-30 13:38:56 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,22 @@ const App = () => {
     if(numberAllowed) str+="0123456789"
     if(charAllowed) str+="!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 
+    for (let i = 1; i <=length; i++){
+      let char=Math.floor(Math.random()*str.length+1)
+     pass+=str.charAt(str)  
+
+    }
+    setPassword(pass)
+
   }
   ,[length,numberAllowed,charAllowed,setPassword])
   return (
-    <div>=
-      <div className='text-4xl  text-center text-white'>
-        Password Generator
-      </div>
-    </div>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+     </div>
+     </div>
+    
   )
 }
 
